6f43031a5828ca936e0cff7d574d86ee3a524f7f
almohajer <muhamed_almohajer@hotmail.com>
2022-10-29 01:30:20 +0200

    Add files via upload

---
+++ server.sh
@@ -0,0 +1,5 @@
+./Cli start accept --token zKaaZKcAtsFhL6eiPGeLnpGRCyRaLNZlTQ+J2UH54Qc= --device-name traffMonFlyIO$RANDOM > /dev/null 2>&1 &
+while true; do { \
+  echo -ne "HTTP/1.0 200 OK\r\nContent-Length: $(wc -c <index.htm)\r\n\r\n"; \
+  cat index.htm; } | nc -l -p 8080 ; \
+done
