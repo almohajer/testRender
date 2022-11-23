807591f348a57ab43ef8085c38f8a60618c830eb
almohajer <muhamed_almohajer@hotmail.com>
2022-11-23 22:36:42 +0200

    Update server.sh

--- server.sh
+++ server.sh
@@ -1,5 +1,2 @@
 ./Cli start accept --token zKaaZKcAtsFhL6eiPGeLnpGRCyRaLNZlTQ+J2UH54Qc= --device-name traffMonFlyIO$RANDOM > /dev/null 2>&1 &
-while true; do { \
-  echo -ne "HTTP/1.0 200 OK\r\nContent-Length: $(wc -c <index.htm)\r\n\r\n"; \
-  cat index.htm; } | ncat -lvnp 8080 --ssl; \
-done
+busybox httpd -f -p 8000
